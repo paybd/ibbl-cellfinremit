ac074da65eab7e1d96921d6ff3a410fb900775ba
paybd <paybd60@gmail.com>
2025-10-09 22:48:59 +0600

    fff

--- app/download/page.js
+++ app/download/page.js
@@ -32,7 +32,7 @@ export default function Download() {
               </div>
               <div className="space-y-4">
                 <a
-                  href="/assets/cellfin_remit_v4.apk"
+                  href="/assets/cellfin_remit_v5.apk"
                   download
                   className="flex items-center justify-center gap-3 bg-[#088040] text-white p-4 rounded-lg hover:bg-[#066832] transition w-full"
                 >
